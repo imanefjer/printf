2676cd48395af9da0b444116f9df8a1ba6d90911
imanefjer <imane.fjer@gmail.com>
2023-04-25 15:07:35 -0700

    fix betty code

--- _printf.c
+++ _printf.c
@@ -125,31 +125,30 @@ int  print_C(va_list arg)
  */
 void print_I(va_list arg)
 {
-        int i ;
-        char str[12];
-        char output[12];
-        int j = 0 ;
-        int k = 0;
-        int negative = 0;
-        i = va_arg(arg, int);
-
-        if (i < 0)
-        {
-                negative = 1;
-                i = -i ;
-        }
-        do {
-                str[j++] = i% 10 +'0';
-                i /= 10;
-        } while (i > 0);
-
-        if (negative == 1) {
-                str[j++] ='-';
-        }
-        for(k = 0; k < j; k++){
-                output[k] = str[j-k-1];
-        }
-        output[j] = '\0';
-
-        write(1,output, j);
+	int i;
+	char str[12];
+	char output[12];
+	int j = 0;
+	int k = 0;
+	int negative = 0;
+	i = va_arg(arg, int);
+	if (i < 0)
+	{
+		negative = 1;
+		i = -i;
+	}
+	do {
+		str[j++] = i % 10 + '0';
+		i /= 10;
+	} while (i > 0);
+	if (negative == 1)
+	{
+		str[j++] = '-';
+	}
+	for (k = 0; k < j; k++)
+	{
+		output[k] = str[j - k - 1];
+	}
+	output[j] = '\0';
+	write(1, output, j);
 }
