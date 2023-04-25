1fbaa3e0298e5527a6e0da35dc06e8e595bdf1bb
imane fjer <93489936+imanefjer@users.noreply.github.com>
2023-04-25 22:49:06 +0100

    add print_I

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@
 int length(char str[]);
 int print_S(va_list arg);
 int print_C(va_list arg);
-
+void print_I(va_list arg);
 /**
  * _printf - Printf function
  * @format: format.
@@ -20,7 +20,8 @@ int _printf(const char *format, ...)
 	int check = 0;
 	format_W formats[] = {
 		{"s", print_S},
-		{"c", print_C}
+		{"c", print_C},
+		 {"i", print_I}
 	};
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
@@ -35,7 +36,7 @@ int _printf(const char *format, ...)
 		{
 			i = 0;
 			check = 0;
-			while (i < 2)
+			while (i < 3)
 			{
 				if (format[j + 1] == *formats[i].format)
 				{
@@ -116,4 +117,39 @@ int  print_C(va_list arg)
 	if (c > 0)
 		write(1, &c, 1);
 	return (1);
+}
+/**
+ * print_I - prints an integer
+ * @arg: Argument
+ * Return: nothing
+ */
+void print_I(va_list arg)
+{
+        int i ;
+        char str[12];
+        char output[12];
+        int j = 0 ;
+        int k = 0;
+        int negative = 0;
+        i = va_arg(arg, int);
+
+        if (i < 0)
+        {
+                negative = 1;
+                i = -i ;
+        }
+        do {
+                str[j++] = i% 10 +'0';
+                i /= 10;
+        } while (i > 0);
+
+        if (negative == 1) {
+                str[j++] ='-';
+        }
+        for(k = 0; k < j; k++){
+                output[k] = str[j-k-1];
+        }
+        output[j] = '\0';
+
+        write(1,output, j);
 }
