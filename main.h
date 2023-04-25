54fe97e96f155f6778b03e1379891c808b0a744d
ela-hidri <elahidri.eh@gmail.com>
2023-04-25 06:51:07 -0700

    adding main.h

--- main.h
+++ main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+typedef struct format
+{
+	char *format;
+	void (*f)(va_list arg);
+} format_W;
+
 int _printf(const char *format, ...);
 #endif
