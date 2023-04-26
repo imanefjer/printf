f639a0b8123265aa058bc5b4ca1ff1df0b8bbd55
imanefjer <imane.fjer@gmail.com>
2023-04-25 17:26:14 -0700

    fix some prob

--- _printf.c
+++ _printf.c
@@ -29,6 +29,10 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
+		return (-1);
+	}
 	va_start(arg, format);
 
 	while (format[j] != '\0')
