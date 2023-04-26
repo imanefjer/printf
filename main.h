f3a18e19d0291ed0846998790e40c968b3e373ec
imanefjer <imane.fjer@gmail.com>
2023-04-25 19:11:56 -0700

    fix some prob

--- main.h
+++ main.h
@@ -2,16 +2,18 @@
 #define MAIN_H
 
 #include <stdarg.h>
+
 /**
  * stuct formats - formats
  * @format: format that are possible
- * @f pointer to the printing function
+ * @f: pointer to the printing function
  */
 typedef struct formats
 {
 	char *format;
 	int (*f)(va_list arg);
 } format_W;
+
 int length(char str[]);
 int print_S(va_list arg);
 int print_C(va_list arg);
@@ -19,4 +21,3 @@ int _printf(const char *format, ...);
 int print_d(va_list arg);
 int print_I(va_list arg);
 #endif
-
